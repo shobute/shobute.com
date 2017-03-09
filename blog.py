21dd0f6b1b01a7d5938842bee43f03adb5cf2f27
Ben Lloyd <ben@shobute.com>
2017-03-09 19:27:50 +0000

    Added missing files from last commit.

---
+++ blog.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import os
+
+from flask import Flask, render_template, request
+from jinja2 import ChoiceLoader, FileSystemLoader
+
+
+app = Flask(__name__)
+
+app.jinja_loader = ChoiceLoader([
+    app.jinja_loader,
+    FileSystemLoader(['templates', 'posts']),
+])
+
+
+@app.route('/')
+def index():
+    page = int(request.args.get('page', 0))
+    return render_template('index.html', posts=getPosts(page), page=page)
+
+
+@app.route('/<slug>')
+def showPost(slug=None):
+    try:
+        post = getPost(slug)
+        return render_template('index.html', posts=[post], page=-1)
+    except IOError:
+        return not_found('Could not find ' + slug)
+
+
+def getPost(slug):
+    file = 'posts/' + slug + '.html'
+    if not os.path.isfile(file):
+        raise IOError
+    return {
+        'slug': slug,
+        'updated': datetime.fromtimestamp(os.path.getmtime(file)),
+    }
+
+
+def getPosts(page):
+    page_size = 10
+
+    files = os.listdir('posts/')
+    posts = []
+    for file in files:
+        if file[0] != '.':
+            posts.append(getPost(file.replace('.html', '')))
+    posts = sorted(posts, key=lambda post: post['updated'], reverse=True)
+
+    posts_from = page_size * page
+    posts_to = posts_from + page_size
+    return posts[posts_from:posts_to]
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
